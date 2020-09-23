da3c8846fda92b7a566b002a86ce1179d8ffe7c3
Kane Knight <kane.knight@hotmail.com>
2020-09-23 20:07:55 +0100

    delete handler implemenented.

--- handlers.go
+++ handlers.go
@@ -7,6 +7,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 	"context"
+	"net/http"
 )
 
 func CreateNoteHandler(c* gin.Context) {
@@ -42,20 +43,49 @@ func UpdateNoteHandler(c* gin.Context) {
 	filter := bson.M{"_id": bson.M{"$eq": noteID}}
 	update := bson.M{"$set": bson.M{"content": note.Content}}
 
-	_, err := client.Database("thirdfort").Collection("notes").UpdateOne(context.TODO(), filter, update)
+	res, err := client.Database("thirdfort").Collection("notes").UpdateOne(context.TODO(), filter, update)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	c.JSON(200, gin.H{
-		"status"  : "Note updated.",
-		"content" : note.Content,
-		"noteID"  : noteID,
-		"userID"  : note.UserID})
+	if res.MatchedCount == 0 {
+		c.AbortWithStatusJSON(http.StatusNotFound,  gin.H{
+			"status": "error",
+			"data": gin.H{
+				"message": "Note does not exist so I can't update it.",
+			},
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"status"  : "Note updated.",
+			"content" : note.Content,
+			"noteID"  : noteID,
+			"userID"  : note.UserID})
+	}
 }
 
 func DeleteNoteHandler(c* gin.Context) {
-	
+	client := c.MustGet("client").(*mongo.Client)
+	noteID, _ := primitive.ObjectIDFromHex(c.Param("id"))
+
+	filter := bson.M{"_id": bson.M{"$eq": noteID}}
+
+	res, err := client.Database("thirdfort").Collection("notes").DeleteOne(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.DeletedCount == 0 {
+		c.AbortWithStatusJSON(http.StatusNotFound,  gin.H{
+			"status": "error",
+			"data": gin.H{
+				"message": "Note does not exist so I can't delete it.",
+			},
+		})	
+	} else {
+		c.JSON(200, gin.H{
+		"status"  : "Note deleted."})
+	}	
 }
 
 func ArchiveNoteHandler(c* gin.Context) {
