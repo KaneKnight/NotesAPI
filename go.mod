8ae5bd5a949a17e6cb1777019a37ddd792781efc
Kane Knight <kane.knight@hotmail.com>
2020-09-23 16:03:10 +0100

    register and login paths defined.

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module ThirdfortAPI
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	github.com/yasaricli/gah v0.0.0-20200110175914-1d93173de233 // indirect
+	go.mongodb.org/mongo-driver v1.4.1 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+)
--- server.go
+++ server.go
@@ -1,15 +1,53 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "net/http"
-
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/yasaricli/gah"
+	"context"
+    "fmt"
+    "log"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+  )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", home)
+
+	//Connect()
+	
+	r.POST("/login", gah.LoginHandler)
+    r.POST("/register", gah.RegisterHandler)
 	r.Run(":8080")
 }
 
-func home(c* gin.Context) {
-	c.String(http.StatusOK, "Hello.")
+
+
+func Connect() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)	
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("thirdfort").Collection("users")
+
+	_, err = collection.InsertOne(context.TODO(), gah.LoginStruct{"kaneywaney11@hotmail.co.uk", "12345"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Close Connection
+	err = client.Disconnect(context.TODO())
+	if err != nil {
+   	 log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
 }
